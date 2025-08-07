75f7e322026a6ce44317ca6db5bb5f16d90ae7da
vadim.kucheryavy@katemedia.eu <zTX4aR2}35*7N1@O>
2025-08-07 21:32:29 +0300

    SECURITY: Remove sensitive data and add config.example.js

---
+++ config.example.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BOT_TOKEN: 'YOUR_BOT_TOKEN_HERE', // Получите у @BotFather
+  ADMIN_ID: 123456789, // Ваш Telegram ID (получите у @userinfobot)
+  REMINDER_HOURS: 2 // За сколько часов напоминать о записи
+}; 
